ad6c203248954bd37c136c59077a1205b11f9491
yixihan <yixihan20010617@gmail.com>
2022-04-04 14:32:13 +0800

    22-04-04

--- src/com/yixihan/test/test01/Main.java
+++ src/com/yixihan/test/test01/Main.java
@@ -8,12 +8,25 @@ public class Main {
 
     public static void main(String[] args) {
         Employee w1 = new Manager ();
-        Manager w2 = (Manager) w1;
+//        Manager w2 = (Manager) w1;
+        System.out.println (w1.getClass ());
+        Manager w2 = null;
 
-        System.out.println (w1.name);
-        w1.getName1 ();
-        System.out.println ("============");
-        System.out.println (w2.name);
-        w2.getName2 ();
+        if (w1 instanceof Manager) {
+            w2 = (Manager) w1;
+        }
+        System.out.println (w2.getClass ());
+
+        Employee w3 = new Employee ();
+
+        if (w3 instanceof Manager) {
+            w2 = (Manager) w3;
+        }
+
+//        System.out.println (w1.name);
+//        w1.getName1 ();
+//        System.out.println ("============");
+//        System.out.println (w2.name);
+//        w2.getName2 ();
     }
 }
