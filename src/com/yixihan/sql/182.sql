795e513eade9d08a556d16378569e3f95ced0a83
yixihan <yixihan20010617@gmail.com>
2022-04-08 13:26:06 +0800

    22-04-08

--- src/com/yixihan/sql/182.sql
+++ src/com/yixihan/sql/182.sql
@@ -38,3 +38,8 @@ select distinct p1.Email
 from Person as p1
          left join Person as p2 on p1.Id != p2.Id
 where p1.Email = p2.Email;
+
+select distinct p1.Email
+from Person as p1
+         left join Person as p2 on p1.Email = p2.Email and p1.Id != p2.Id
+where p2.Id is not null;
