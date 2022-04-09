3e30c1c6aec902cf2b5a7cd777e97865b92ab741
yixihan <yixihan20010617@gmail.com>
2022-04-09 13:49:51 +0800

    22-04-09

---
+++ src/com/yixihan/sql/596.sql
@@ -0,0 +1,78 @@
+# question
+/*
+ 表: Courses
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class)是该表的主键列。
+该表的每一行表示学生的名字和他们注册的班级。
+
+编写一个SQL查询来报告 至少有5个学生 的所有类。
+
+以 任意顺序 返回结果表。
+
+查询结果格式如下所示。
+
+输入: 
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+输出: 
++---------+ 
+| class   | 
++---------+ 
+| Math    | 
++---------+
+解释: 
+-数学课有6个学生，所以我们包括它。
+-英语课有1名学生，所以我们不包括它。
+-生物课有1名学生，所以我们不包括它。
+-计算机课有1个学生，所以我们不包括它。
+ */
+
+# framework
+Create table If Not Exists Courses
+(
+    student varchar(255),
+    class   varchar(255)
+);
+Truncate table Courses;
+insert into Courses (student, class)
+values ('A', 'Math');
+insert into Courses (student, class)
+values ('B', 'English');
+insert into Courses (student, class)
+values ('C', 'Math');
+insert into Courses (student, class)
+values ('D', 'Biology');
+insert into Courses (student, class)
+values ('E', 'Math');
+insert into Courses (student, class)
+values ('F', 'Computer');
+insert into Courses (student, class)
+values ('G', 'Math');
+insert into Courses (student, class)
+values ('H', 'Math');
+insert into Courses (student, class)
+values ('I', 'Math');
+
+# answer
+select class
+from Courses
+group by class
+having count(student) >= 5;
