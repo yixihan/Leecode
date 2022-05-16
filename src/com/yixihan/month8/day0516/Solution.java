e63066547cbb62451ad198e9a7b47675e6c3374c
易曦翰 <yixihan20010617@gmail.com>
2022-05-16 14:03:09 +0800

    22-05-16

---
+++ src/com/yixihan/month8/day0516/Solution.java
@@ -0,0 +1,54 @@
+package com.yixihan.month8.day0516;
+
+/**
+ * 设计一个算法，找出二叉搜索树中指定节点的“下一个”节点（也即中序后继）。
+ * <p>
+ * 如果指定节点没有对应的“下一个”节点，则返回null。
+ * <p>
+ * 输入: root = [2,1,3], p = 1
+ * <p>
+ * 2
+ * / \
+ * 1   3
+ * <p>
+ * 输出: 2
+ * <p>
+ * 输入: root = [5,3,6,2,4,null,null,1], p = 6
+ * <p>
+ * 5
+ * / \
+ * 3   6
+ * / \
+ * 2   4
+ * /
+ * 1
+ * <p>
+ * 输出: null
+ *
+ * @author : yixihan
+ * @create : 2022-05-16-13:56
+ */
+public class Solution {
+
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null) {
+            return null;
+        }
+        if (p.val >= root.val) {
+            return inorderSuccessor (root.right, p);
+        }
+
+        TreeNode node = inorderSuccessor (root.left, p);
+        return node == null ? root : node;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
