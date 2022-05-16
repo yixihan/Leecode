87d1c2a4815b3cc245a572b2e752a309fc9d52e7
易曦翰 <yixihan20010617@gmail.com>
2022-05-16 14:27:09 +0800

    22-05-16

---
+++ src/com/yixihan/binary_2/day8/searchMatrix.java
@@ -0,0 +1,36 @@
+package com.yixihan.binary_2.day8;
+
+/**
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+ * <p>
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
+ * <p>
+ * 输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+ * 输出：true
+ * <p>
+ * 输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+ * 输出：false
+ *
+ * @author : yixihan
+ * @create : 2022-05-16-14:05
+ */
+public class searchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = m - 1, j = 0; i >= 0 && j < n; ) {
+            if (matrix[i][j] > target) {
+                i--;
+            } else if (matrix[i][j] < target) {
+                j++;
+            } else {
+                return true;
+            }
+        }
+
+
+        return false;
+    }
+}
