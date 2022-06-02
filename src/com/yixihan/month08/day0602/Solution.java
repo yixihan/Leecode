b19b56e74c902331405ed5531131c0e34daff048
易曦翰 <yixihan20010617@gmail.com>
2022-06-02 13:19:59 +0800

    22-06-02

---
+++ src/com/yixihan/month08/day0602/Solution.java
@@ -0,0 +1,94 @@
+package com.yixihan.month08.day0602;
+
+/**
+ * 给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，并保证二叉搜索树的性质不变。
+ * 返回二叉搜索树（有可能被更新）的根节点的引用。
+ * <p>
+ * 一般来说，删除节点可分为两个步骤：
+ * <p>
+ * 首先找到需要删除的节点；
+ * 如果找到了，删除它。
+ * <p>
+ * 输入：root = [5,3,6,2,4,null,7], key = 3
+ * 输出：[5,4,6,2,null,null,7]
+ * 解释：给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+ * 一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+ * 另一个正确答案是 [5,2,6,null,4,null,7]。
+ * <p>
+ * 输入: root = [5,3,6,2,4,null,7], key = 0
+ * 输出: [5,3,6,2,4,null,7]
+ * 解释: 二叉树不包含值为 0 的节点
+ * <p>
+ * 输入: root = [], key = 0
+ * 输出: []
+ *
+ * @author : yixihan
+ * @create : 2022-06-02-13:19
+ */
+public class Solution {
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == key) {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else {
+                TreeNode ans = min (root.right);
+                ans.right = deleteMin (root.right);
+                ans.left = root.left;
+                return ans;
+            }
+        } else if (root.val < key) {
+            root.right = deleteNode (root.right, key);
+            return root;
+        } else {
+            root.left = deleteNode (root.left, key);
+            return root;
+        }
+
+
+    }
+
+    private TreeNode min(TreeNode node) {
+        if (node.left != null) {
+            return min (node.left);
+        }
+
+        return node;
+    }
+
+    private TreeNode deleteMin(TreeNode node) {
+        if (node.left == null) {
+            return node.right;
+        }
+
+        node.left = deleteMin (node.left);
+        return node;
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
