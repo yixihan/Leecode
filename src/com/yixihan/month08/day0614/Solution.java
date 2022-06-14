c60a103af4de128e5e50d4af4eafed7da338b33c
易曦翰 <yixihan20010617@gmail.com>
2022-06-15 06:51:27 +0800

    22-06-15

--- src/com/yixihan/month08/day0614/Solution.java
+++ src/com/yixihan/month08/day0614/Solution.java
@@ -1,52 +1,51 @@
 package com.yixihan.month08.day0614;
 
-import java.util.Arrays;
-
 /**
- * 学校打算为全体学生拍一张年度纪念照。根据要求，学生需要按照 非递减 的高度顺序排成一行。
+ * 给你一个大小为 m x n 的矩阵 mat ，请以对角线遍历的顺序，用一个数组返回这个矩阵中的所有元素。
  * <p>
- * 排序后的高度情况用整数数组 expected 表示，其中 expected[i] 是预计排在这一行中第 i 位的学生的高度（下标从 0 开始）。
+ * 输入：mat = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[1,2,4,7,5,3,6,8,9]
+ * 示例 2：
  * <p>
- * 给你一个整数数组 heights ，表示 当前学生站位 的高度情况。heights[i] 是这一行中第 i 位学生的高度（下标从 0 开始）。
- * <p>
- * 返回满足 heights[i] != expected[i] 的 下标数量 。
- * <p>
- * 输入：heights = [1,1,4,2,1,3]
- * 输出：3
- * 解释：
- * 高度：[1,1,4,2,1,3]
- * 预期：[1,1,1,2,3,4]
- * 下标 2 、4 、5 处的学生高度不匹配。
- * <p>
- * 输入：heights = [5,1,2,3,4]
- * 输出：5
- * 解释：
- * 高度：[5,1,2,3,4]
- * 预期：[1,2,3,4,5]
- * 所有下标的对应学生高度都不匹配。
- * <p>
- * 输入：heights = [1,2,3,4,5]
- * 输出：0
- * 解释：
- * 高度：[1,2,3,4,5]
- * 预期：[1,2,3,4,5]
- * 所有下标的对应学生高度都匹配。
+ * 输入：mat = [[1,2],[3,4]]
+ * 输出：[1,2,3,4]
  *
  * @author : yixihan
- * @create : 2022-06-14-11:59
+ * @create : 2022-06-14-12:05
  */
 public class Solution {
-    public int heightChecker(int[] heights) {
-        int[] expected = Arrays.copyOf (heights, heights.length);
-        Arrays.sort (expected);
-        int ans = 0;
 
-        for (int i = 0; i < heights.length; i++) {
-            if (heights[i] != expected[i]) {
-                ans++;
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] ans = new int[m * n];
+        int index = 0;
+        int i = 0;
+        int j = 0;
+
+        while (index < m * n) {
+            ans[index++] = mat[i][j];
+
+            if ((i + j) % 2 == 0) {
+                if (j == n - 1) {
+                    i++;
+                } else if (i == 0) {
+                    j++;
+                } else {
+                    i--;
+                    j++;
+                }
+            } else {
+                if (i == m - 1) {
+                    j++;
+                } else if (j == 0) {
+                    i++;
+                } else {
+                    i++;
+                    j--;
+                }
             }
         }
-
         return ans;
     }
 }
--- src/com/yixihan/month08/day0615/Solution.java
+++ src/com/yixihan/month08/day0615/Solution.java
@@ -1,51 +1,78 @@
 package com.yixihan.month08.day0615;
 
+import java.util.Arrays;
+
 /**
- * 给你一个大小为 m x n 的矩阵 mat ，请以对角线遍历的顺序，用一个数组返回这个矩阵中的所有元素。
+ * 数对 (a,b) 由整数 a 和 b 组成，其数对距离定义为 a 和 b 的绝对差值。
  * <p>
- * 输入：mat = [[1,2,3],[4,5,6],[7,8,9]]
- * 输出：[1,2,4,7,5,3,6,8,9]
- * 示例 2：
+ * 给你一个整数数组 nums 和一个整数 k ，数对由 nums[i] 和 nums[j] 组成且满足 0 <= i < j < nums.length 。返回 所有数对距离中 第 k 小的数对距离。
  * <p>
- * 输入：mat = [[1,2],[3,4]]
- * 输出：[1,2,3,4]
+ * 输入：nums = [1,3,1], k = 1
+ * 输出：0
+ * 解释：数对和对应的距离如下：
+ * (1,3) -> 2
+ * (1,1) -> 0
+ * (3,1) -> 2
+ * 距离第 1 小的数对是 (1,1) ，距离为 0 。
+ * <p>
+ * 输入：nums = [1,1,1], k = 2
+ * 输出：0
+ * <p>
+ * 输入：nums = [1,6,1], k = 3
+ * 输出：5
  *
  * @author : yixihan
- * @create : 2022-06-14-12:05
+ * @create : 2022-06-15-6:49
  */
 public class Solution {
 
-    public int[] findDiagonalOrder(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        int[] ans = new int[m * n];
-        int index = 0;
-        int i = 0;
-        int j = 0;
+    private int[] nums;
+    private int n;
 
-        while (index < m * n) {
-            ans[index++] = mat[i][j];
+    public int smallestDistancePair(int[] nums, int k) {
+        //排序
+        Arrays.sort (nums);
 
-            if ((i + j) % 2 == 0) {
-                if (j == n - 1) {
-                    i++;
-                } else if (i == 0) {
-                    j++;
-                } else {
-                    i--;
-                    j++;
-                }
-            } else {
-                if (i == m - 1) {
-                    j++;
-                } else if (j == 0) {
-                    i++;
-                } else {
-                    i++;
-                    j--;
-                }
+        this.nums = nums;
+        this.n = this.nums.length;
+
+        // 二分查找的左右边界
+        int left = 0;
+        int right = this.nums[n - 1] - this.nums[0];
+
+        // 二分查找
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int count = lessThanMidCount (mid);
+
+            if (count >= k) {
+                right = mid - 1;
+                continue;
             }
+
+            left = mid + 1;
         }
-        return ans;
+
+        return left;
+    }
+
+    /**
+     * 小于mid的数对个数
+     * 双指针算法
+     *
+     * @param mid 标定值
+     * @return 数对个数
+     */
+    private int lessThanMidCount(int mid) {
+        int count = 0;
+
+        for (int i = 0, j = 0; j < n; j++) {
+            while (nums[j] - nums[i] > mid) {
+                i++;
+            }
+            count += j - i;
+        }
+
+        return count;
     }
 }
