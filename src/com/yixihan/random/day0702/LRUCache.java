a9f98c435f52a484e7a0f77c402133f38c496029
易曦翰 <yixihan20010617@gmail.com>
2022-07-02 09:30:52 +0800

    22-07-02

---
+++ src/com/yixihan/random/day0702/LRUCache.java
@@ -0,0 +1,60 @@
+package com.yixihan.random.day0702;
+
+import java.util.*;
+
+/**
+ * 运用所掌握的数据结构，设计和实现一个  LRU (Least Recently Used，最近最少使用) 缓存机制 。
+ * <p>
+ * 实现 LRUCache 类：
+ * <p>
+ * LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存
+ * int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+ * void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。
+ * 当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+ * <p>
+ * 输入
+ * ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+ * [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+ * 输出
+ * [null, null, null, 1, null, -1, null, -1, 3, 4]
+ * <p>
+ * 解释
+ * LRUCache lRUCache = new LRUCache(2);
+ * lRUCache.put(1, 1); // 缓存是 {1=1}
+ * lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+ * lRUCache.get(1);    // 返回 1
+ * lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+ * lRUCache.get(2);    // 返回 -1 (未找到)
+ * lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+ * lRUCache.get(1);    // 返回 -1 (未找到)
+ * lRUCache.get(3);    // 返回 3
+ * lRUCache.get(4);    // 返回 4
+ *
+ * @author : yixihan
+ * @create : 2022-07-02-8:55
+ */
+public class LRUCache {
+
+    LinkedHashMap<Integer, Integer> map;
+    int size;
+
+    public LRUCache(int capacity) {
+        map = new LinkedHashMap<Integer, Integer> (capacity + 1, 0.75F, true) {
+            @Override
+            protected boolean removeEldestEntry (Map.Entry<Integer, Integer> var) {
+                return size() > size;
+            }
+        };
+        size = capacity;
+    }
+
+    public int get(int key) {
+        return map.getOrDefault (key, -1);
+    }
+
+    public void put(int key, int value) {
+        map.put (key, value);
+    }
+
+
+}
