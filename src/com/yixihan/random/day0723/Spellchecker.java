f39650b7606fc5028c96517b322b8819702c384d
易曦翰 <yixihan20010617@gmail.com>
2022-07-23 10:02:50 +0800

    22-07-23

---
+++ src/com/yixihan/random/day0723/Spellchecker.java
@@ -0,0 +1,76 @@
+package com.yixihan.random.day0723;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 在给定单词列表 wordlist 的情况下，我们希望实现一个拼写检查器，将查询单词转换为正确的单词。
+ * <p>
+ * 对于给定的查询单词 query，拼写检查器将会处理两类拼写错误：
+ * <p>
+ * 大小写：如果查询匹配单词列表中的某个单词（不区分大小写），则返回的正确单词与单词列表中的大小写相同。
+ * 例如：wordlist = ["yellow"], query = "YellOw": correct = "yellow"
+ * 例如：wordlist = ["Yellow"], query = "yellow": correct = "Yellow"
+ * 例如：wordlist = ["yellow"], query = "yellow": correct = "yellow"
+ * 元音错误：如果在将查询单词中的元音 ('a', 'e', 'i', 'o', 'u')  分别替换为任何元音后，能与单词列表中的单词匹配（不区分大小写），
+ * 则返回的正确单词与单词列表中的匹配项大小写相同。
+ * 例如：wordlist = ["YellOw"], query = "yollow": correct = "YellOw"
+ * 例如：wordlist = ["YellOw"], query = "yeellow": correct = "" （无匹配项）
+ * 例如：wordlist = ["YellOw"], query = "yllw": correct = "" （无匹配项）
+ * 此外，拼写检查器还按照以下优先级规则操作：
+ * <p>
+ * 当查询完全匹配单词列表中的某个单词（区分大小写）时，应返回相同的单词。
+ * 当查询匹配到大小写问题的单词时，您应该返回单词列表中的第一个这样的匹配项。
+ * 当查询匹配到元音错误的单词时，您应该返回单词列表中的第一个这样的匹配项。
+ * 如果该查询在单词列表中没有匹配项，则应返回空字符串。
+ * 给出一些查询 queries，返回一个单词列表 answer，其中 answer[i] 是由查询 query = queries[i] 得到的正确单词。
+ * <p>
+ * 输入：wordlist = ["KiTe","kite","hare","Hare"], queries = ["kite","Kite","KiTe","Hare","HARE","Hear","hear","keti","keet","keto"]
+ * 输出：["kite","KiTe","KiTe","Hare","hare","","","KiTe","","KiTe"]
+ * <p>
+ * 输入：wordlist = ["yellow"], queries = ["YellOw"]
+ * 输出：["yellow"]
+ *
+ * @author : yixihan
+ * @create : 2022-07-23-9:32
+ */
+public class Spellchecker {
+
+    public String[] spellchecker(String[] wordlist, String[] queries) {
+        Map<String, String> wordMap = new HashMap<> ();
+        Map<String, String> wordPatternMap = new HashMap<> ();
+        for (String word : wordlist) {
+            wordMap.put (word, word);
+            wordPatternMap.putIfAbsent (word.toLowerCase (), word);
+            wordPatternMap.putIfAbsent (wordPattern (word), word);
+        }
+        for (int i = 0; i < queries.length; i++) {
+            if (wordMap.containsKey (queries[i])) {
+                queries[i] = wordMap.get (queries[i]);
+                continue;
+            }
+            String wordPattern = queries[i].toLowerCase ();
+            if (wordPatternMap.containsKey (wordPattern)) {
+                queries[i] = wordPatternMap.get (wordPattern);
+                continue;
+            }
+            wordPattern = wordPattern (queries[i]);
+            if (wordPatternMap.containsKey (wordPattern)) {
+                queries[i] = wordPatternMap.get (wordPattern);
+                continue;
+            }
+            queries[i] = "";
+        }
+        return queries;
+    }
+
+    private String wordPattern(String word) {
+        char[] letters = word.toLowerCase ().toCharArray ();
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] == 'a' || letters[i] == 'e' || letters[i] == 'i' || letters[i] == 'o' || letters[i] == 'u') {
+                letters[i] = '*';
+            }
+        }
+        return String.valueOf (letters);
+    }
+}
---
+++ src/com/yixihan/random/day0723/UniquePaths.java
@@ -0,0 +1,48 @@
+package com.yixihan.random.day0723;
+
+/**
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * <p>
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ * <p>
+ * 问总共有多少条不同的路径？
+ * <p>
+ * 输入：m = 3, n = 7
+ * 输出：28
+ * <p>
+ * 输入：m = 3, n = 2
+ * 输出：3
+ * 解释：
+ * 从左上角开始，总共有 3 条路径可以到达右下角。
+ * 1. 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右
+ * 3. 向下 -> 向右 -> 向下
+ * <p>
+ * 输入：m = 7, n = 3
+ * 输出：28
+ *
+ * @author : yixihan
+ * @create : 2022-07-23-9:24
+ */
+public class UniquePaths {
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
