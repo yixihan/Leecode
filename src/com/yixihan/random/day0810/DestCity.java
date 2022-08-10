486fa89181688899472a7c48f961422c2bfa2624
易曦翰 <yixihan20010617@gmail.com>
2022-08-10 10:26:58 +0800

    22-08-10

---
+++ src/com/yixihan/random/day0810/DestCity.java
@@ -0,0 +1,58 @@
+package com.yixihan.random.day0810;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 给你一份旅游线路图，该线路图中的旅行线路用数组 paths 表示，
+ * 其中 paths[i] = [cityAi, cityBi] 表示该线路将会从 cityAi 直接前往 cityBi 。
+ * 请你找出这次旅行的终点站，即没有任何可以通往其他城市的线路的城市。
+ * <p>
+ * 题目数据保证线路图会形成一条不存在循环的线路，因此恰有一个旅行终点站。
+ * <p>
+ * 输入：paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+ * 输出："Sao Paulo"
+ * 解释：从 "London" 出发，最后抵达终点站 "Sao Paulo" 。本次旅行的路线是 "London" -> "New York" -> "Lima" -> "Sao Paulo" 。
+ * <p>
+ * 输入：paths = [["B","C"],["D","B"],["C","A"]]
+ * 输出："A"
+ * 解释：所有可能的线路是：
+ * "D" -> "B" -> "C" -> "A".
+ * "B" -> "C" -> "A".
+ * "C" -> "A".
+ * "A".
+ * 显然，旅行终点站是 "A" 。
+ * 示例 3：
+ * <p>
+ * 输入：paths = [["A","Z"]]
+ * 输出："Z"
+ *
+ * @author : yixihan
+ * @create : 2022-08-10-10:22
+ */
+public class DestCity {
+
+    public String destCity(List<List<String>> paths) {
+        Map<String, List<String>> map = new HashMap<> ();
+        String ans = "";
+        for (List<String> path : paths) {
+            List<String> list1 = map.getOrDefault (path.get (0), new ArrayList<> ());
+            list1.add (path.get (1));
+            map.put (path.get (0), list1);
+
+            if (!map.containsKey (path.get (1))) {
+                map.put (path.get (1), new ArrayList<> ());
+            }
+        }
+
+        for (Map.Entry<String, List<String>> entry : map.entrySet ()) {
+            if (entry.getValue ().isEmpty ()) {
+                ans = entry.getKey ();
+                break;
+            }
+        }
+        return ans;
+    }
+}
