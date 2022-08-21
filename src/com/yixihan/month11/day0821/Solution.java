ed36897e2e55573bee493fa83aff96d788c9e95f
易曦翰 <yixihan20010617@gmail.com>
2022-08-21 08:51:54 +0800

    22-08-21

---
+++ src/com/yixihan/month11/day0821/Solution.java
@@ -0,0 +1,44 @@
+package com.yixihan.month11.day0821;
+
+/**
+ * 给你一个字符串 sentence 作为句子并指定检索词为 searchWord ，其中句子由若干用 单个空格 分隔的单词组成。
+ * 请你检查检索词 searchWord 是否为句子 sentence 中任意单词的前缀。
+ * <p>
+ * 如果 searchWord 是某一个单词的前缀，则返回句子 sentence 中该单词所对应的下标（下标从 1 开始）。
+ * 如果 searchWord 是多个单词的前缀，则返回匹配的第一个单词的下标（最小下标）。
+ * 如果 searchWord 不是任何单词的前缀，则返回 -1 。
+ * <p>
+ * 字符串 s 的 前缀 是 s 的任何前导连续子字符串。
+ * <p>
+ * 输入：sentence = "i love eating burger", searchWord = "burg"
+ * 输出：4
+ * 解释："burg" 是 "burger" 的前缀，而 "burger" 是句子中第 4 个单词。
+ * <p>
+ * 输入：sentence = "this problem is an easy problem", searchWord = "pro"
+ * 输出：2
+ * 解释："pro" 是 "problem" 的前缀，而 "problem" 是句子中第 2 个也是第 6 个单词，但是应该返回最小下标 2 。
+ * <p>
+ * 输入：sentence = "i am tired", searchWord = "you"
+ * 输出：-1
+ * 解释："you" 不是句子中任何单词的前缀。
+ *
+ * @author : yixihan
+ * @create : 2022-08-21-8:42
+ */
+public class Solution {
+
+    public int isPrefixOfWord(String sentence, String searchWord) {
+
+        String[] splits = sentence.split (" ");
+        int ans = -1;
+
+        for (int i = 0; i < splits.length; i++) {
+            if (splits[i].startsWith (searchWord)) {
+                ans = i + 1;
+                break;
+            }
+        }
+
+        return ans;
+    }
+}
