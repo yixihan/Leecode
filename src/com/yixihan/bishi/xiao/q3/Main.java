a5b4757cefc939da595372f2d280f8da98a900b2
易曦翰 <yixihan20010617@gmail.com>
2022-09-14 11:19:52 +0800

    22-09-14

---
+++ src/com/yixihan/bishi/xiao/q3/Main.java
@@ -0,0 +1,32 @@
+package com.yixihan.bishi.xiao.q3;
+
+import java.util.Vector;
+
+/**
+ * @author : yixihan
+ * @create : 2022-09-14-11:04
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        String s = "Metallica";
+
+        Vector<Integer> vector = new Vector<> ();
+        System.out.println ((11 + 3 * 8) / 4 % 3);
+        System.out.println (s.substring (7));
+    }
+}
+
+//class Example {
+//    public static void main(String[] args) {
+//        String val = null;
+//        int x = Integer.parseInt (val);
+//        System.out.println (x);
+//    }
+//}
+
+class Example {
+    public static void main(String[] args) {
+        System.out.println ("string".endsWith (""));
+    }
+}
