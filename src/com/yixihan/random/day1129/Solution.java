c8383b1bc02dc0e9c69e437526aad5e884861d1f
zengsitong <zengsitong@newhope.cn>
2022-11-29 15:26:22 +0800

    22-11-29

---
+++ src/com/yixihan/random/day1129/Solution.java
@@ -0,0 +1,47 @@
+package com.yixihan.random.day1129;
+
+/**
+ * 给你一个字符串 title ，它由单个空格连接一个或多个单词组成，每个单词都只包含英文字母。请你按以下规则将每个单词的首字母 大写 ：
+ * <p>
+ * 如果单词的长度为 1 或者 2 ，所有字母变成小写。
+ * 否则，将单词首字母大写，剩余字母变成小写。
+ * 请你返回 大写后 的 title 。
+ * <p>
+ * 输入：title = "capiTalIze tHe titLe"
+ * 输出："Capitalize The Title"
+ * 解释：
+ * 由于所有单词的长度都至少为 3 ，将每个单词首字母大写，剩余字母变为小写。
+ * <p>
+ * 输入：title = "First leTTeR of EACH Word"
+ * 输出："First Letter of Each Word"
+ * 解释：
+ * 单词 "of" 长度为 2 ，所以它保持完全小写。
+ * 其他单词长度都至少为 3 ，所以其他单词首字母大写，剩余字母小写。
+ * <p>
+ * 输入：title = "i lOve leetcode"
+ * 输出："i Love Leetcode"
+ * 解释：
+ * 单词 "i" 长度为 1 ，所以它保留小写。
+ * 其他单词长度都至少为 3 ，所以其他单词首字母大写，剩余字母小写。
+ *
+ * @author yixihan
+ * @date 2022/11/29 15:24
+ */
+public class Solution {
+    
+    public String capitalizeTitle(String title) {
+        StringBuilder sb = new StringBuilder ();
+        
+        for (String str : title.split (" ")) {
+            if (str.length () == 1 || str.length () == 2) {
+                sb.append (str.toLowerCase ());
+            } else {
+                str = str.substring (0, 1).toUpperCase () + str.substring (1).toLowerCase ();
+                sb.append (str);
+            }
+            sb.append (" ");
+        }
+        
+        return sb.deleteCharAt (sb.length () - 1).toString ();
+    }
+}
